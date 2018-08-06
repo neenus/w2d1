3557a2e40a4792be75b60836d6e099b927fe46a0
Neenus Gabriel <neenus@live.ca>
2018-08-06 20:57:23 +0000

    corrected an error in calculating buffer in step two and implemented step three

--- https-requests/getAndPrintHTML-v2.js
+++ https-requests/getAndPrintHTML-v2.js
@@ -1,23 +1,24 @@
 var https = require('https');
 
-function getAndPrintHTML (options) {
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
 
-    var requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step3.html'
-    };
+function getAndPrintHTMLv2 (options) {
+
   
     /* Add your code here */
-    https.get(requestOptions, function (response) {
+    https.get(options, function (response) {
         var buffer;
         // set encoding to UTF-8
         response.setEncoding('utf8');
 
         // callback is invoked when data chunck is received
         response.on('data', function (data) {
-            buffer += data;
+            buffer += buffer + data;
             console.log(buffer);
         });
     });
   }
-  getAndPrintHTML();
+  getAndPrintHTMLv2(requestOptions);
