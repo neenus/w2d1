4a33d094bb9d03b15bc25b729aae1d662a6044f5
Neenus Gabriel <neenus@live.ca>
2018-08-06 20:11:14 +0000

    HTTP requests - Step two completed

---
+++ https-requests/getAndPrintHTML.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+  
+    /* Add your code here */
+    https.get(requestOptions, function (response) {
+        var buffer;
+        // set encoding to UTF-8
+        response.setEncoding('utf8');
+
+        // callback is invoked when data chunck is received
+        response.on('data', function (data) {
+            buffer = buffer + data;
+            console.log(buffer);
+        });
+    });
+  }
+  getAndPrintHTML();
